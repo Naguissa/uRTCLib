24436919de078d04bc4062505b2648e8ed2a30ae
Prashant Kumar <prash.k20@gmail.com>
2024-01-27 00:10:32 -0800

    Add set_12hour_mode() comment to example, add hourModeAndAmPm() usage
    hourModeAndAmPm() = 0 for 24 hour mode, 1 = AM, 2 = PM
    Model will check whether to read SRAM or not, use alarms or not
    based on rtc model.

--- examples/uRTCLib_example_full/uRTCLib_example_full.ino
+++ examples/uRTCLib_example_full/uRTCLib_example_full.ino
@@ -9,7 +9,9 @@
  *     * RAM for DS1307 and DS3232
  *     * temperature sensor for DS3231 and DS3232
  *     * Alarms (1 and 2) for DS3231 and DS3232
- *     * Power failure check for DS3231 and DS3232
+ *     * Power lost flag reading and clearing
+ *     * Enable Oscillator flag to check if Oscillator will run on VBAT
+ *     * Set Clock in 12 hour or 24 hour mode. Get AM PM if in 12 hour mode. (Alarm set still in 24 hour mode)
  *
  * See uEEPROMLib for EEPROM support.
  *
@@ -25,6 +27,11 @@
 
 uRTCLib rtc;
 
+// URTCLIB_MODEL_DS1307
+// URTCLIB_MODEL_DS3231
+// URTCLIB_MODEL_DS3232
+byte rtcModel = URTCLIB_MODEL_DS3231;
+
 uint8_t position;
 
 void setup() {
@@ -39,11 +46,22 @@ delay (2000);
 		URTCLIB_WIRE.begin();
 	#endif
 	rtc.set_rtc_address(0x68);
-	rtc.set_model(URTCLIB_MODEL_DS3232);
 
-	// Only used once, then disabled
-	rtc.set(0, 42, 16, 6, 2, 5, 15);
-	//  RTCLib::set(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+	// set RTC Model
+	rtc.set_model(rtcModel);
+
+	// refresh data from RTC HW in RTC class object
+	rtc.refresh();
+
+	// Only use once, then disable
+	// rtc.set(0, 42, 16, 6, 2, 5, 15);
+	//  RTCLib::set(byte second, byte minute, byte hour (0-23:24-hr mode only), byte dayOfWeek (Sun = 1, Sat = 7), byte dayOfMonth (1-12), byte month, byte year)
+
+	// use the following if you want to set main clock in 12 hour mode
+	// rtc.set_12hour_mode(true);
+
+	// use the following if you want to set main clock in 24 hour mode
+	// rtc.set_12hour_mode(false);
 
 	if (rtc.enableBattery()) {
 		Serial.println("Battery activated correctly.");
@@ -52,9 +70,10 @@ delay (2000);
 	}
 
 	// Check whether OSC is set to use VBAT or not
-	if (rtc.getEOSCFlag()) {
+	if (rtc.getEOSCFlag())
 		Serial.println(F("Oscillator will not use VBAT when VCC cuts off. Time will not increment without VCC!"));
-	}
+	else
+		Serial.println(F("Oscillator will use VBAT when VCC cuts off."));
 
 	Serial.print("Lost power status: ");
 	if (rtc.lostPower()) {
@@ -108,12 +127,14 @@ delay (2000);
 		rtc.ramWrite(position, position);
 	}
 
-	rtc.alarmClearFlag(URTCLIB_ALARM_1);
-	rtc.alarmClearFlag(URTCLIB_ALARM_2);
+	if(rtcModel == URTCLIB_MODEL_DS3231 || rtcModel == URTCLIB_MODEL_DS3232) {
+		rtc.alarmClearFlag(URTCLIB_ALARM_1);
+		rtc.alarmClearFlag(URTCLIB_ALARM_2);
 
-	// Alarm pin will get low at sny :10 seconds. We clear the flag on loop to be able to see it, as it doesn't clear itself when triggered
-	rtc.alarmSet(URTCLIB_ALARM_TYPE_1_FIXED_S, 10, 0, 0, 1); // Each minute, at just :10 seconds
-	// RTCLib::alarmSet(uint8_t type, uint8_t second, uint8_t minute, uint8_t hour, uint8_t day_dow);
+		// Alarm pin will get low at sny :10 seconds. We clear the flag on loop to be able to see it, as it doesn't clear itself when triggered
+		rtc.alarmSet(URTCLIB_ALARM_TYPE_1_FIXED_S, 10, 0, 0, 1); // Each minute, at just :10 seconds
+		// RTCLib::alarmSet(uint8_t type, uint8_t second, uint8_t minute, uint8_t hour, uint8_t day_dow);
+	}
 
 	position = 0;
 
@@ -136,6 +157,10 @@ void loop() {
 	Serial.print(rtc.minute());
 	Serial.print(':');
 	Serial.print(rtc.second());
+	if(rtc.hourModeAndAmPm() == 1)
+		Serial.print(" AM");
+	if(rtc.hourModeAndAmPm() == 2)
+		Serial.print(" PM");
 
 	Serial.print(" DOW: ");
 	Serial.print(rtc.dayOfWeek());
@@ -143,18 +168,20 @@ void loop() {
 	Serial.print(" - Temp: ");
 	Serial.print((float) rtc.temp() / 100);
 
-	Serial.print(" - SRAM position ");
-	Serial.print(position);
-	Serial.print(" value: ");
-	Serial.print(rtc.ramRead(position), HEX);
+	if(rtcModel == URTCLIB_MODEL_DS1307 || rtcModel == URTCLIB_MODEL_DS3232) {
+		Serial.print(" - SRAM position ");
+		Serial.print(position);
+		Serial.print(" value: ");
+		Serial.print(rtc.ramRead(position), HEX);
+		position++;
+	}
 
 	Serial.println();
 
-	rtc.alarmClearFlag(URTCLIB_ALARM_1);
-	rtc.alarmClearFlag(URTCLIB_ALARM_2);
-
-
-	position++;
+	if(rtcModel == URTCLIB_MODEL_DS3231 || rtcModel == URTCLIB_MODEL_DS3232) {
+		rtc.alarmClearFlag(URTCLIB_ALARM_1);
+		rtc.alarmClearFlag(URTCLIB_ALARM_2);
+	}
 
 	delay(1000);
 }
