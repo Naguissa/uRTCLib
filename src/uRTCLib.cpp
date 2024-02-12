3933e2769e68af14d90d96f29ab8122ef45ba60e
Naguissa <naguissa@foroelectro.net>
2024-02-12 10:46:54 +0100

    Add error check to refresh function. Now this function returns bool value, false on errors: PR #36 Thanks @BenUniqcode

--- src/uRTCLib.cpp
+++ src/uRTCLib.cpp
@@ -24,7 +24,7 @@
  * @see <a href="https://www.foroelectro.net/librerias-arduino-ide-f29/rtclib-arduino-libreria-simple-y-eficaz-para-rtc-y-t95.html">https://www.foroelectro.net/librerias-arduino-ide-f29/rtclib-arduino-libreria-simple-y-eficaz-para-rtc-y-t95.html</a>
  * @see <a href="mailto:naguissa@foroelectro.net">naguissa@foroelectro.net</a>
  * @see <a href="https://github.com/Naguissa/uEEPROMLib">See uEEPROMLib for EEPROM support.</a>
- * @version 6.8.0
+ * @version 6.9.0
  */
 
 #include <Arduino.h>
@@ -69,6 +69,8 @@ uRTCLib::uRTCLib(const int rtc_address, const uint8_t model) {
 
 /**
  * \brief Refresh data from HW RTC
+ *
+ * @return False on error
  */
 bool uRTCLib::refresh() {
 	uRTCLIB_YIELD
--- src/uRTCLib.h
+++ src/uRTCLib.h
@@ -21,7 +21,7 @@
  * @see <a href="https://www.foroelectro.net/librerias-arduino-ide-f29/rtclib-arduino-libreria-simple-y-eficaz-para-rtc-y-t95.html">https://www.foroelectro.net/librerias-arduino-ide-f29/rtclib-arduino-libreria-simple-y-eficaz-para-rtc-y-t95.html</a>
  * @see <a href="mailto:naguissa@foroelectro.net">naguissa@foroelectro.net</a>
  * @see <a href="https://github.com/Naguissa/uEEPROMLib">See uEEPROMLib for EEPROM support.</a>
- * @version 6.8.0
+ * @version 6.9.0
  */
 /** \file uRTCLib.h
  *   \brief uRTCLib header file
@@ -317,6 +317,8 @@
 			/******* RTC functions ********/
 			/**
 			 * \brief Refresh data from HW RTC
+			 *
+			 * @return False on error
 			 */
 			bool refresh();
 			/**
