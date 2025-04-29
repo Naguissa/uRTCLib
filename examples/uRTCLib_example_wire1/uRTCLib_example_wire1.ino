8e776b1abc45e5657fd027ff467cf735edc92c91
Naguissa <naguissa@foroelectro.net>
2025-04-29 11:34:23 +0200

    Add Wire1 example and Arduino-ci

---
+++ examples/uRTCLib_example_wire1/uRTCLib_example_wire1.ino
@@ -0,0 +1,74 @@
+/**
+ * DS1307, DS3231 and DS3232 RTCs basic library
+ *
+ * Really tiny library to basic RTC functionality on Arduino.
+ *
+ * Supported features:
+ *     * SQuare Wave Generator
+ *     * Fixed output pin for DS1307
+ *     * RAM for DS1307 and DS3232
+ *     * temperature sensor for DS3231 and DS3232
+ *     * Alarms (1 and 2) for DS3231 and DS3232
+ *     * Power failure check for DS3231 and DS3232
+ *
+ * See uEEPROMLib for EEPROM support.
+ *
+ * @copyright Naguissa
+ * @author Naguissa
+ * @url https://github.com/Naguissa/uRTCLib
+ * @url https://www.foroelectro.net/librerias-arduino-ide-f29/rtclib-arduino-libreria-simple-y-eficaz-para-rtc-y-t95.html
+ * @email naguissa@foroelectro.net
+ */
+#include "Arduino.h"
+#include <Wire.h>
+#define URTCLIB_WIRE Wire1
+#include "uRTCLib.h"
+
+
+// uRTCLib rtc;
+uRTCLib rtc(0x68);
+
+
+void setup() {
+delay (2000);
+	Serial.begin(9600);
+	Serial.println("Serial OK");
+
+	#ifdef ARDUINO_ARCH_ESP8266
+		URTCLIB_WIRE.begin(0, 2); // D3 and D4 on ESP8266
+	#else
+		URTCLIB_WIRE.begin();
+	#endif
+
+	rtc.set(0, 42, 16, 6, 2, 5, 15);
+	//  RTCLib::set(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+}
+
+void loop() {
+	rtc.refresh();
+
+	Serial.print("RTC DateTime: ");
+	Serial.print(rtc.year());
+	Serial.print('/');
+	Serial.print(rtc.month());
+	Serial.print('/');
+	Serial.print(rtc.day());
+
+	Serial.print(' ');
+
+	Serial.print(rtc.hour());
+	Serial.print(':');
+	Serial.print(rtc.minute());
+	Serial.print(':');
+	Serial.print(rtc.second());
+
+	Serial.print(" DOW: ");
+	Serial.print(rtc.dayOfWeek());
+
+	Serial.print(" - Temp: ");
+	Serial.print(rtc.temp()  / 100);
+
+	Serial.println();
+
+	delay(1000);
+}
